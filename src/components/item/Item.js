8fb86872eb2aafa78d60e89debf5f225210a99fa
eyyup <eyyupordueri@gmail.com>
2022-07-18 18:16:41 +0300

    File was updated.

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -1,12 +1,36 @@
+import { useState, useEffect } from 'react';
 import "./Item.css";
 
 const Item = ({card}) => {
+    const [showLogo, setShowLogo] = useState(true);
+
     const {name, img, options} = card;
 
+    const handleClick = () => {
+        showLogo ? setShowLogo(!showLogo) : 
+        setShowLogo(!showLogo);
+    }
+
+    useEffect (() => {
+        if (!showLogo) {
+            setTimeout(() => setShowLogo(!showLogo), 3000);
+        }}, [showLogo]);
+    
   return (
-    <div className="card">
-        <img className="card-logo" src={img} alt="lang-logo" />
-        <h1 className="card-title"> {name}</h1>
+    <div className="card" onClick={handleClick}>
+        {
+            showLogo ? (
+                <div>
+                    <img className="card-logo" src={img} alt="lang-logo" />
+                    <h1 className="card-title"> {name}</h1>
+                </div>
+            ) : (
+                <ul className='list'>
+                    {options.map((element, index) => {
+                    return <li key={index}>{element}</li>
+                })}
+                </ul>
+            )}
     </div>
   )
 }
